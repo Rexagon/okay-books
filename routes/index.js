aa9747052f041cd4970b5e38bdac02af8cc6e4a8
Flextwer <reide740@gmail.com>
2016-09-28 22:08:05 +0300

    Auto commit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get('/', function(req, res) {
     res.render('index');
 });
 
+router.get('/googlec7bdfe46dbeeb9c2.html', function(req, res) {
+    res.render('googlec7bdfe46dbeeb9c2');
+});
+
+
 module.exports = router;
