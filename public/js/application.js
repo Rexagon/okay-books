e2ea5755e4db282460c0ec30dc388104b338929c
Flextwer <reide740@gmail.com>
2016-09-28 21:27:31 +0300

    Auto commit

---
+++ public/js/application.js
@@ -0,0 +1,19 @@
+$('#submit').on('click', function() {
+    var form = $('#application-form').serializeArray();
+    var application = {};
+    for (var i = 0; i < form.length; ++i) {
+        application[form[i].name] = form[i].value;
+    }
+    if (application.name == '' || application.phone == '' || application.email == '') {
+        alert('Пожалуйста укажите Ф.И.О, номер телефона и адрес электронной почты.');
+    } else {
+        $.post('/application', application, function(res) {
+            if (!res.err) {
+                $(':input', '#application-form').not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');
+                alert('Ваша заявка на расчёт издания была отправлена! Мы свяжемся с Вами после её обработки.');
+            } else if (res.err == 'email') {
+                alert('Введите действительный адрес электронной почты.');
+            }
+        });
+    }
+});
